a6eaf5a948ce0052bf0b4255a974626f4689f821
June823 <idahjunecherotich@gmail.com>
2025-11-20 13:46:20 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,19 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
-import { Provider  } from 'react-redux';
+import { Provider } from 'react-redux';
 import { store } from './store/store';
 
+// Create root
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </Provider>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Performance measuring
 reportWebVitals();
