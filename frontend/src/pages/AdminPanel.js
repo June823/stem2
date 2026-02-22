58b2b0673ca9072a956af15a0568dfaf0f82c248
June823 <idahjunecherotich@gmail.com>
2026-02-22 13:19:21 +0300

    Update AdminPanel.js

--- frontend/src/pages/AdminPanel.js
+++ frontend/src/pages/AdminPanel.js
@@ -13,41 +13,33 @@ const AdminPanel = () => {
     if (user && user.role !== ROLE.ADMIN) {
       navigate("/");
     }
-  }, [user]);
+  }, [user, navigate]);
 
   return (
     <div className="min-h-[calc(100vh-120px)] md:flex">
-      <aside className="bg-white min-h-full w-full max-w-60 customShadow">
-        <div className="h-32 flex justify-center items-center flex-col">
-          <div className="text-5xl flex justify-center">
-            {user?.profilePic ? (
-              <img
-                src={getImageUrl(user.profilePic)}
-                className="w-20 h-20 rounded-full"
-                alt={user?.name}
-              />
-            ) : (
-              <FaRegCircleUser />
-            )}
-          </div>
+      <aside className="bg-white w-full max-w-60 shadow">
+        <div className="h-32 flex flex-col justify-center items-center">
+          {user?.profilePic ? (
+            <img
+              src={getImageUrl(user.profilePic)}
+              className="w-20 h-20 rounded-full"
+              alt={user?.name}
+            />
+          ) : (
+            <FaRegCircleUser size={50} />
+          )}
 
-          <p className="capitalize text-lg font-semibold">
-            {user?.name}
-          </p>
+          <p className="font-semibold">{user?.name}</p>
           <p className="text-sm">{user?.role}</p>
         </div>
 
-        <nav className="grid p-4">
-          <Link to={"all-users"} className="px-2 py-1 hover:bg-slate-100">
-            All Users
-          </Link>
-          <Link to={"all-products"} className="px-2 py-1 hover:bg-slate-100">
-            All Products
-          </Link>
+        <nav className="p-4 grid gap-2">
+          <Link to="all-users">All Users</Link>
+          <Link to="all-products">All Products</Link>
         </nav>
       </aside>
 
-      <main className="w-full h-full p-2">
+      <main className="flex-1 p-4">
         <Outlet />
       </main>
     </div>
