671a5dd8a4d754f27a0ae279686ab81d662a32d1
June823 <idahjunecherotich@gmail.com>
2026-02-22 12:17:18 +0300

    Update userDetails.js

--- backend/controllers/user/userDetails.js
+++ backend/controllers/user/userDetails.js
@@ -1,25 +1,23 @@
-const userModel = require("../../models/userModel")
+const userModel = require("../../models/userModel");
 
 async function userDetailsController(req, res) {
   try {
-    console.log("userId", req.userId)
-    const user = await userModel.findById(req.userId)
+    const user = await userModel.findById(req.userId);
 
-    res.status(200).json({
+    return res.status(200).json({
       data: user,
-      error: false,
       success: true,
-      message: "User details"
-    })
+      error: false,
+      message: "User details",
+    });
 
-    console.log("user", user)
   } catch (err) {
-    res.status(400).json({
-      message: err.message || err,
+    return res.status(400).json({
+      message: err.message,
+      success: false,
       error: true,
-      success: false
-    })
+    });
   }
 }
 
-module.exports = userDetailsController
+module.exports = userDetailsController;
