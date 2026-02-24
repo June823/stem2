697c3e35e51b2fb76ebbce80528d6eb743f0ce42
June823 <idahjunecherotich@gmail.com>
2026-02-24 17:33:39 +0300

    Update displayCurrency.js

--- frontend/src/helpers/displayCurrency.js
+++ frontend/src/helpers/displayCurrency.js
@@ -1,12 +1,19 @@
-const displayINRCurrency = (num) => {
-    const formatter = new Intl.NumberFormat('en-IN',{
-        style : "currency",
-        currency : 'INR',
-        minimumFractionDigits : 2
-    })
+const displayKShCurrency = (num) => {
+    // Ensure num is a valid number to avoid "NaN"
+    const value = Number(num);
+    
+    if (isNaN(value)) {
+        return "KSh 0.00";
+    }
 
-    return formatter.format(num)
+    const formatter = new Intl.NumberFormat('en-KE', {
+        style: "currency",
+        currency: 'KES', // Kenyan Shilling Code
+        minimumFractionDigits: 0 // KSh usually doesn't show cents
+    });
 
+    // Replace the default "KES" symbol with "KSh" if preferred
+    return formatter.format(value).replace("KES", "KSh");
 }
 
-export default displayINRCurrency
+export default displayKShCurrency;
